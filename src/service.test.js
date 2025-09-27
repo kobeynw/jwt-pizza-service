a60326ccd36e9e7c5f3bc9efe1a9a2942f25f0a6
Kobey Workman <kobeyworkman1@gmail.com>
2025-09-27 15:27:47 -0600

    Add service tests that require admin privileges

--- src/service.test.js
+++ src/service.test.js
@@ -111,3 +111,146 @@ test('logout', async () => {
   const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
   expect(logoutRes.body.message).toBe('logout successful');
 });
+
+test('update menu', async () => {
+  const newItem = {
+    'title': 'Dessert',
+    'image': 'pizza9.png',
+    'price': 10.59,
+    'description': 'Something delightfully sweet',
+  };
+  const orderRes = await request(app).put('/api/order/menu').set('Authorization', `Bearer ${adminUserAuthToken}`).send(newItem);
+  expect(orderRes.status).toBe(200);
+  expect(orderRes.body.id).toBeInteger;
+  expect(orderRes.body.title).toBeString;
+  expect(orderRes.body.price).toBeNumber;
+  expect(orderRes.body.description).toBeString;
+});
+
+test('create order', async () => {
+  const newOrder = {
+    'franchiseId': 1,
+    'storeId': 1,
+    'items': [
+      {
+        'menuId': 1,
+        'description': 'Something delightfully sweet',
+        'price': 10.59,
+      },
+    ],
+  };
+  const orderRes = await request(app).post('/api/order').set('Authorization', `Bearer ${adminUserAuthToken}`).send(newOrder);
+  expect(orderRes.status).toBe(200);
+  expect(orderRes.body.order).toMatchObject(newOrder);
+  expect(orderRes.body.jwt).toBeString;
+});
+
+test('get order', async () => {
+  const orderRes = await request(app).get('/api/order').set('Authorization', `Bearer ${adminUserAuthToken}`);
+  expect(orderRes.status).toBe(200);
+  orderRes.body.orders.forEach(order => {
+    expect(order.id).toBeInteger;
+    expect(order.franchiseId).toBeInteger;
+    expect(order.storeId).toBeInteger;
+    expect(order.date).toMatch(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/);
+
+    order.items.forEach(item => {
+      expect(item.id).toBeInteger;
+      expect(item.menuId).toBeInteger;
+      expect(item.price).toBeNumber;
+      expect(item.description).toBeString;
+    });
+  });
+});
+
+test('create franchise', async () => {
+  const newFranchise = {
+    'name': Math.random().toString(36).substring(2, 12),
+    'admins': [
+      {
+        'email': testUser.email,
+      },
+    ],
+  };
+  const franchiseRes = await request(app).post('/api/franchise').set('Authorization', `Bearer ${adminUserAuthToken}`).send(newFranchise);
+  expect(franchiseRes.status).toBe(200);
+  expect(franchiseRes.body.name).toBe(newFranchise.name);
+  expect(franchiseRes.body.id).toBeInteger;
+  franchiseRes.body.admins.forEach(admin => {
+    expect(admin.name).toBe(testUser.name);
+    expect(admin.email).toBe(testUser.email);
+    expect(admin.id).toBe(testUser.id);
+  });
+  franchiseId = franchiseRes.body.id;
+});
+
+test('create store', async () => {
+  const newStore = {
+    'franchiseId': franchiseId,
+    'name': 'SLC'
+  };
+  const storeRes = await request(app).post(`/api/franchise/${franchiseId}/store`).set('Authorization', `Bearer ${adminUserAuthToken}`).send(newStore);
+  expect(storeRes.status).toBe(200); 
+  expect(storeRes.body.id).toBeInteger;
+  expect(storeRes.body.name).toBe(newStore.name);
+  expect(storeRes.body.totalRevenue).toBeNumber;
+  storeId = storeRes.body.id;
+});
+
+test('get franchises', async () => {
+  const franchiseRes = await request(app).get('/api/franchise?page=0&limit=10&name=*');
+  expect(franchiseRes.status).toBe(200);
+  franchiseRes.body.franchises.forEach(franchise => {
+    expect(franchise.id).toBeInteger;
+    expect(franchise.name).toBeString;
+    
+    franchise.admins?.forEach(admin => {
+      expect(admin.id).toBeInteger;
+      expect(admin.name).toBeString;
+      expect(admin.email).toBeString;
+    });
+
+    franchise.stores.forEach(store => {
+      expect(store.id).toBeInteger;
+      expect(store.name).toBeString;
+      expect(store.totalRevenue).toBeNumber;
+    });
+  });
+});
+
+test('get user franchises', async () => {
+  const franchiseRes = await request(app).get(`/api/franchise/${testUser.id}`).set('Authorization', `Bearer ${adminUserAuthToken}`);
+  expect(franchiseRes.status).toBe(200);
+  franchiseRes.body.forEach(franchise => {
+    expect(franchise.id).toBeInteger;
+    expect(franchise.name).toBeString;
+    
+    franchise.admins?.forEach(admin => {
+      expect(admin.id).toBeInteger;
+      expect(admin.name).toBeString;
+      expect(admin.email).toBeString;
+    });
+
+    franchise.stores.forEach(store => {
+      expect(store.id).toBeInteger;
+      expect(store.name).toBeString;
+      expect(store.totalRevenue).toBeNumber;
+    });
+  });
+});
+
+test('delete store', async () => {
+  const storeRes = await request(app).delete(`/api/franchise/${franchiseId}/store/${storeId}`).set('Authorization', `Bearer ${adminUserAuthToken}`);
+  expect(storeRes.status).toBe(200);
+  expect(storeRes.body.message).toBe('store deleted');
+});
+
+test('delete franchise', async () => {
+  const franchiseRes = await request(app).delete(`/api/franchise/${franchiseId}`).set('Authorization', `Bearer ${adminUserAuthToken}`);
+  expect(franchiseRes.status).toBe(200);
+  expect(franchiseRes.body.message).toBe('franchise deleted');
+});
+
+afterAll(async () => {
+  await request(app).delete('/api/auth').set('Authorization', `Bearer ${adminUserAuthToken}`);
+});
